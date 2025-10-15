f9eb45b77197807b2f576a323cd4bdc6e2fc84c9
sj-minRva <rheamaria13@gmail.com>
2025-10-15 15:19:10 +0530

    practice

---
+++ Practice/Epsilon Closure.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define MAX 20
+
+int transitions[MAX][3][20], transCount=0;
+int closure[MAX][20], closureCount=0;
+
+
+void findClosure(const char *state)
+{
+    for(int i=0; i<transCount; i++)
+    {
+        if(transitions[i][0] == )
+    }
+}
+
+void isClosure(const char *state)
+{
+
+}
+
+void addClosure(const char *state)
+{
+
+}
+
+
+
+int main()
+{
+
+}
+
