cf71611a298aadde0749d40fcdd31bfdf60be778
sj-minRva <rheamaria13@gmail.com>
2025-11-19 14:40:15 +0530

    first and follow

--- code/fandf.c
+++ code/fandf.c
@@ -9,13 +9,10 @@ void FIRST(char c, int first[]);
 void FOLLOW(char c, int follow[]);
 
 int main() {
-    int i, cont;
-    char ch;
-
+    int i, ch;
     printf("Enter number of productions: ");
     scanf("%d", &n);
-
-    printf("Enter productions (use -> for production and $ for epsilon):\n");
+    printf("Enter productions (E=TX format):\n");
     for (i = 0; i < n; i++)
         scanf("%s", prod[i]);
 
@@ -39,25 +36,20 @@ int main() {
         printf("}\n");
 
         printf("Continue? (1/0): ");
-        scanf("%d", &cont);
-    } while (cont == 1);
+        scanf("%d", &i);
+    } while (i == 1);
 
     return 0;
 }
 
 void FIRST(char c, int first[]) {
-    if (!isupper(c)) {
-        first[c] = 1;
-        return;
-    }
-
+    if (!isupper(c)) { first[c] = 1; return; }
     for (int i = 0; i < n; i++) {
         if (prod[i][0] == c) {
-            if (prod[i][3] == '$')
+            if (prod[i][2] == '$')  // epsilon production
                 first['$'] = 1;
-            else {
-                FIRST(prod[i][3], first);
-            }
+            else
+                FIRST(prod[i][2], first);
         }
     }
 }
@@ -66,23 +58,25 @@ void FOLLOW(char c, int follow[]) {
     if (prod[0][0] == c)
         follow['$'] = 1;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         int len = strlen(prod[i]);
-        for (int j = 3; j < len; j++) {
-            if (prod[i][j] == c) {
-                if (prod[i][j + 1] != '\0') {
+        for (int j = 2; j < len; j++) 
+        {
+            if (prod[i][j] == c) 
+            {
+                if (prod[i][j + 1] != '\0') 
+                {
                     int firstNext[256] = {0};
                     FIRST(prod[i][j + 1], firstNext);
-
                     for (int k = 0; k < 256; k++)
                         if (firstNext[k] && k != '$')
                             follow[k] = 1;
-
                     if (firstNext['$'])
                         FOLLOW(prod[i][0], follow);
-                } else if (prod[i][0] != c) {
+                } 
+                else if (prod[i][0] != c)
                     FOLLOW(prod[i][0], follow);
-                }
             }
         }
     }
